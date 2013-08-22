9f902e8e705a444961f81df5f55840544b7cf936
fuhao715 <fuhao715@gmail.com>
2013-08-22 20:59:15 +0800

    main函数

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"github.com/astaxie/beego"
+	"mygos/mytesting"
+)
+
+type MainController struct {
+	beego.Controller
+}
+
+func (this *MainController) Get() {
+	this.Ctx.WriteString("hello world")
+}
+
+func main() {
+	fmt.Println("begin :")
+
+	mytesting.PrintBeegoVersion()
+	fmt.Println("----------")
+	beego.Router("/", &MainController{})
+	beego.Run()
+}
